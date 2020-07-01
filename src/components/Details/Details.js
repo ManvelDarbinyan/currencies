bf2992f9bd68fb303ddeb8b5d5edd191615e636f
Manvel Darbinyan <manveldarbinyan@gmail.com>
2020-07-01 20:15:43 +0400

    currencies

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -3,81 +3,156 @@ import "./Details.css";
 import fetchService from "../../services/FetchService";
 import Loading from "../Loading/Loading";
 import renderChangePercent from "../../helpers/renderChangePercent";
+import { useEffect, useReducer, useCallback } from "react";
 
-class Details extends React.PureComponent {
-  constructor() {
-    super();
-    this.state = {
-      loading: true,
-      currency: {},
-    };
+// class Details extends React.PureComponent {
+//   constructor() {
+//     super();
+//     this.state = {
+//       loading: true,
+//       currency: {},
+//     };
+//   }
+
+//   getCurrency = async () => {
+//     const currencyId = this.props.match.params.id;
+//     const response = await fetchService.get(`cryptocurrencies/${currencyId}`);
+//     this.setState({
+//       currency: response,
+//       loading: false,
+//     });
+//   };
+
+//   componentDidUpdate(prevProps) {
+//     if (prevProps.match.params.id !== this.props.match.params.id) {
+//       this.getCurrency();
+//     }
+//   }
+
+//   componentDidMount() {
+//     this.getCurrency();
+//   }
+
+//   render() {
+//     const { loading, currency } = this.state;
+
+//     if (loading) {
+//       return (
+//         <div className="loading-container">
+//           <Loading />
+//         </div>
+//       );
+//     }
+//     return (
+//       <div className="Detail">
+//         <h1 className="Detail-heading">
+//           {currency.name} ({currency.symbol})
+//         </h1>
+//         <div className="Detail-container">
+//           <div className="Detail-item">
+//             Price <span className="Detail-value">$ {currency.price}</span>
+//           </div>
+//           <div className="Detail-item">
+//             Rank <span className="Detail-value">{currency.rank}</span>
+//           </div>
+//           <div className="Detail-item">
+//             24H Change
+//             <span className="Detail-value">
+//               {renderChangePercent(currency.percentChange24h)}
+//             </span>
+//           </div>
+//           <div className="Detail-item">
+//             <span className="Detail-title">Market cap</span>
+//             <span className="Detail-dollar">$</span>
+//             {currency.marketCap}
+//           </div>
+//           <div className="Detail-item">
+//             <span className="Detail-title">24H Volume</span>
+//             <span className="Detail-dollar">$</span>
+//             {currency.volume24h}
+//           </div>
+//           <div className="Detail-item">
+//             <span className="Detail-title">Total supply</span>
+//             {currency.totalSupply}
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+const detailReducer = function (state, action) {
+  switch (action.type) {
+    case "GET_CRYPTOCURRENCY_DONE":
+      return {
+        loading: false,
+        currency: action.payload,
+      };
   }
+};
 
-  getCurrency = async () => {
-    const currencyId = this.props.match.params.id;
+const Details = ({ match }) => {
+  const [{ currency, loading }, dispatch] = useReducer(detailReducer, {
+    loading: true,
+    currency: {},
+  });
+
+  const getCurrency = useCallback(async () => {
+    const currencyId = match.params.id;
     const response = await fetchService.get(`cryptocurrencies/${currencyId}`);
-    this.setState({
-      currency: response,
-      loading: false,
+    dispatch({
+      type: "GET_CRYPTOCURRENCY_DONE",
+      payload: response,
     });
-  };
+  }, [match.params.id]);
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.match.params.id !== this.props.match.params.id) {
-      this.getCurrency();
-    }
-  }
+  useEffect(() => {
+    getCurrency();
+  }, [match.params.id]);
 
-  componentDidMount() {
-    this.getCurrency();
-  }
-
-  render() {
-    const { loading, currency } = this.state;
-
-    if (loading) {
-      return (
-        <div className="loading-container">
-          <Loading />
-        </div>
-      );
-    }
+  if (loading) {
     return (
-      <div className="Detail">
-        <h1 className="Detail-heading">
-          {currency.name} ({currency.symbol})
-        </h1>
-        <div className="Detail-container">
-          <div className="Detail-item">
-            Price <span className="Detail-value">$ {currency.price}</span>
-          </div>
-          <div className="Detail-item">
-            Rank <span className="Detail-value">{currency.rank}</span>
-          </div>
-          <div className="Detail-item">
-            24H Change
-            <span className="Detail-value">
-              {renderChangePercent(currency.percentChange24h)}
-            </span>
-          </div>
-          <div className="Detail-item">
-            <span className="Detail-title">Market cap</span>
-            <span className="Detail-dollar">$</span>
-            {currency.marketCap}
-          </div>
-          <div className="Detail-item">
-            <span className="Detail-title">24H Volume</span>
-            <span className="Detail-dollar">$</span>
-            {currency.volume24h}
-          </div>
-          <div className="Detail-item">
-            <span className="Detail-title">Total supply</span>
-            {currency.totalSupply}
-          </div>
-        </div>
+      <div className="loading-container">
+        <Loading />
       </div>
     );
   }
-}
+
+  return (
+    <div className="Detail">
+      <h1 className="Detail-heading">
+        {currency.name} ({currency.symbol})
+      </h1>
+      <div className="Detail-container">
+        <div className="Detail-item">
+          Price <span className="Detail-value">$ {currency.price}</span>
+        </div>
+        <div className="Detail-item">
+          Rank <span className="Detail-value">{currency.rank}</span>
+        </div>
+        <div className="Detail-item">
+          24H Change
+          <span className="Detail-value">
+            {renderChangePercent(currency.percentChange24h)}
+          </span>
+        </div>
+        <div className="Detail-item">
+          <span className="Detail-title">Market cap</span>
+          <span className="Detail-dollar">$</span>
+          {currency.marketCap}
+        </div>
+        <div className="Detail-item">
+          <span className="Detail-title">24H Volume</span>
+          <span className="Detail-dollar">$</span>
+          {currency.volume24h}
+        </div>
+        <div className="Detail-item">
+          <span className="Detail-title">Total supply</span>
+          {currency.totalSupply}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Details;
--- src/index.js
+++ src/index.js
@@ -4,19 +4,23 @@ import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import NotFound from "./components/NotFound/NotFound";
-import "./index.css";
 import Details from "./components/Details/Details";
+import { Provider } from "react-redux";
+import store from "./state-management/store/store";
+import "./index.css";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <Switch>
-        <Route path="/" component={List} exact />
-        <Route path="/currency/:id" component={Details} />
-        <Route component={NotFound} />
-      </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/" component={List} exact />
+          <Route path="/currency/:id" component={Details} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
